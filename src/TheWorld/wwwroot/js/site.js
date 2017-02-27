f69fcd8ad695ea29dfd4700c058c659711179ed0
sk399 <saurabh.mysore@gmail.com>
2017-02-27 11:24:21 +0530

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿/*site.js*/
+
+(function () {
+    var ele = $("#username").text("saurabh");
+    //debugger;
+
+    var main = $("#main");
+
+    main.on("mouseenter", function () {
+        main.css("background-color", "#888");
+    });
+    main.on("mouseleave", function () {
+        main.css("background-color", "");
+    });
+    $("#toggleButton").on("click", function () {
+        $("#sidebar,#wrapper").toggleClass("hide-sidebar");
+        if ($("#sidebar,#wrapper").hasClass("hide-sidebar")) {
+            $("#toggleButton").text("show sidebar");
+        }
+        else {
+            $("#toggleButton").text("hide sidebar");
+        }
+    });
+
+})();
